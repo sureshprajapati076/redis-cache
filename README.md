a521cfa38f02066ed22766da73c1eee78cfc1b16
Suresh Prajapati <51094657+sureshprajapati076@users.noreply.github.com>
2020-11-11 11:22:55 -0600

    changes for aws fargate

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ start redis server using **brew services start redis**
 redis-cli monitor in cli
 
 clear cache using **redis-cli FLUSHDB**
+
+for remote redis:
+
+```sh
+redis-cli -u redis://<ip>:<port>
+```
--- src/main/java/com/example/demo/Config.java
+++ src/main/java/com/example/demo/Config.java
@@ -1,30 +1,38 @@
 package com.example.demo;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.context.annotation.*;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
-
 @Configuration
-@ComponentScan("com.example.demo")
-@EnableRedisRepositories(basePackages = "com.example.demo")
-@PropertySource("classpath:application.properties")
+@EnableRedisRepositories
 public class Config {
 
     @Bean
-    JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
+    public JedisConnectionFactory jedisConnectionFactory() {
+        RedisProperties properties = redisProperties();
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        configuration.setHostName(properties.getHost());
+        configuration.setPort(properties.getPort());
+
+        return new JedisConnectionFactory(configuration);
     }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate() {
-        final RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
+        final RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(jedisConnectionFactory());
-        template.setValueSerializer(new GenericToStringSerializer<Object>(Object.class));
+        template.setValueSerializer(new GenericToStringSerializer<>(Object.class));
         return template;
     }
+
+    @Bean
+    @Primary
+    public RedisProperties redisProperties() {
+        return new RedisProperties();
+    }
+
 }
